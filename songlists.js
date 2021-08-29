fd190417b2c786999de0396267bf168fbc2a19e6
aknkz2021 <ks8367etmtcrsrc@gmail.com>
2021-08-29 13:46:52 +0900

    選曲完了

--- songlists.js
+++ songlists.js
@@ -26,64 +26,57 @@ const wedsong = {
   //新郎新婦入場
   ent :  [
     {
-      artist : "The Corrs",
-      title : "Breathless"
+      artist : "Maroon 5",
+      title : "Sugar",
+      mUrl : "https://youtu.be/09R8_2nJtjg"
     },
     {
-      artist : "entartist2",
-      title : "enttitle2"
+      artist : "Carly Rae Jepsen",
+      title : "I Really Like You",
+      mUrl : "https://youtu.be/qV5lzRHrGeg"
     },
     {
-      artist : "entartist2",
-      title : "enttitle2"
+      artist : "Meja",
+      title : "How Crazy Are You?",
+      mUrl : "https://youtu.be/tm4nY7I_rXg"
     }
   ],
   
   //ケーキ入刀
   cake :  [
     {
-      artist : "The Cranberries",
-      title : "Dreams"
+      artist : "MISIA",
+      title : "幸せをフォーエバー",
+      mUrl : "https://youtu.be/pkUsU5h2vag"
     },
     {
-      artist : "cakeartist2",
-      title : "caketitle2"
+      artist : "Owl City & Carly Rae Jepsen",
+      title : "Good Time",
+      mUrl : "https://youtu.be/H7HmzwI67ec"
     },
     {
-      artist : "entartist2",
-      title : "enttitle2"
+      artist : "シェネル",
+      title : "ベイビー・アイラブユー",
+      mUrl : "https://youtu.be/vutqHflemlk"
     }
   ],
 
     //乾杯
   cheers : [
     {
-    artist : "The Cranberries",
-    title : "Dreams"
+    artist : "AI",
+    title : "ハピネス",
+    mUrl : "https://youtu.be/uKgmu8g-kXE"
   },
   {
-    artist : "cakeartist2",
-    title : "caketitle2"
+    artist : "Little Glee Monster",
+    title : "世界はあなたに笑いかけている",
+    mUrl : "https://youtu.be/JKhw0CKEhMk"
   },
   {
-    artist : "entartist2",
-    title : "enttitle2"
-  }
-  ],
-
-  //歓談
-  talktime : [
-    {
-    artist : "The Cranberries",
-    title : "Dreams"
-  },
-  {
-    artist : "cakeartist2",
-    title : "caketitle2"
-  },
-  {
-    artist : "entartist2",
-    title : "enttitle2"
+    artist : "大滝詠一",
+    title : "君は天然色",
+    mUrl : "https://youtu.be/L-hyY-1luHs"
   }
   ],
 
@@ -95,92 +88,109 @@ const wedsong = {
     mUrl: "https://youtu.be/dElRVQFqj-k"
   },
   {
-    artist : "The Cranberries",
-    title : "Dreams"
+    artist : "星野源",
+    title : "Family Song",
+    mUrl : "https://youtu.be/Ucbp-Ej5RpI"
   },
   {
-    artist : "entartist2",
-    title : "enttitle2"
+    artist : "Awesome City Club",
+    title : "ceremony",
+    mUrl : "https://youtu.be/09R8_2nJtjg"
   }
   ],
 
   //お色直し入場
   reEnt : [
     {
-    artist : "AI",
-    title : "Story"
+    artist : "Ariana Grande, John Legend",
+    title : "Beauty and the Beast",
+    mUrl : "https://youtu.be/axySrE0Kg6k"
   },
   {
-    artist : "The Cranberries",
-    title : "Dreams"
+    artist : "Mrs. GREEN APPLE",
+    title : "Love me, Love you",
+    mUrl : "https://youtu.be/FmDBhP4apbs"
   },
   {
-    artist : "entartist2",
-    title : "enttitle2"
+    artist : "三浦大知",
+    title : "Antelope",
+    mUrl : "https://youtu.be/Vl3xWKMM2TU"
   }
   ],
 
   //エンドロール
   endroll : [
     {
-    artist : "小田和正",
-    title : "言葉にできない"
+    artist : "Blue",
+    title : "The Gift",
+    mUrl : "https://youtu.be/WjW8dAcTy60"
   },
   {
-    artist : "cakeartist2",
-    title : "caketitle2"
+    artist : "JUJU",
+    title : "やさしさで溢れるように",
+    mUrl : "https://youtu.be/jIl8_jMMULs"
   },
   {
-    artist : "entartist2",
-    title : "enttitle2"
+    artist : "絢香",
+    title : "ありがとうの輪",
+    mUrl : "https://youtu.be/Tcg_TDM1Enc"
   }
   ],
 
   //両親への手紙
   letter : [
     {
-    artist : "The Cranberries",
-    title : "Dreams"
+    artist : "back number",
+    title : "手紙",
+    mUrl : "https://youtu.be/woRV5VxJDkU"
   },
   {
-    artist : "cakeartist2",
-    title : "caketitle2"
+    artist : "Bank Band",
+    title : "糸",
+    mUrl : "https://youtu.be/1m2QPUq6K8U"
   },
   {
-    artist : "entartist2",
-    title : "enttitle2"
+    artist : "レミオロメン",
+    title : "3月9日",
+    mUrl : "https://youtu.be/UNRJyBHziX4"
   }
   ],
 
   //両親への花束贈呈
-  flowor : [
+  flower : [
     {
-    artist : "山口百恵",
-    title : "秋桜"
+    artist : "Superfly",
+    title : "愛をこめて花束を",
+    mUrl : "https://youtu.be/gU5oN0KVofU"
   },
   {
-    artist : "cakeartist2",
-    title : "caketitle2"
+    artist : "秦 基博",
+    title : "ひまわりの約束",
+    mUrl : "https://youtu.be/rKsQ-3N-Bks"
   },
   {
-    artist : "entartist2",
-    title : "enttitle2"
+    artist : "福山雅治",
+    title : "家族になろうよ",
+    mUrl : "https://youtu.be/vrkbf9CVkn4"
   }
   ],
 
   //お披楽喜
   final : [
     {
-    artist : "福山雅治",
-    title : "家族になろうよ"
+    artist : "Official髭男dism",
+    title : "115万キロのフィルム",
+    mUrl : "https://youtu.be/ReCnlwVZj1M"
   },
   {
-    artist : "cakeartist2",
-    title : "caketitle2"
+    artist : "星野源",
+    title : "恋",
+    mUrl : "https://youtu.be/jhOVibLEDhA"
   },
   {
-    artist : "entartist2",
-    title : "enttitle2"
+    artist : "Rake",
+    title : "100万回の「I love you」",
+    mUrl : "https://youtu.be/_6h1UACCeIU"
   }
   ]
 
